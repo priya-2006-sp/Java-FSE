e4c21e0a56f084a3a1ba5aa5768b35617984111b
SHANMUGAPRIYA S <priyashanmuga783@gmail.com>
2026-06-01 20:42:50 +0530

    Add files via upload

---
+++ upskilling/Module 3/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    public void displayDetails() {
+        System.out.println("Car Details - Make: " + make + ", Model: " + model + ", Year: " + year);
+    }
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Camry", 2022);
+        Car car2 = new Car("Honda", "Civic", 2023);
+        car1.displayDetails();
+        car2.displayDetails();
+    }
+}
